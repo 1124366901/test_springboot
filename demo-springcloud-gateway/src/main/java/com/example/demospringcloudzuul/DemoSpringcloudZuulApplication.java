283d94df5e6bd147854db7b1f06e02dc7db3ed8e
七濑小胡桃 <1124366901@qq.com>
2023-09-11 18:45:07 +0800

    gateway网关注册

---
+++ demo-springcloud-gateway/src/main/java/com/example/demospringcloudzuul/DemoSpringcloudZuulApplication.java
@@ -0,0 +1,11 @@
+package com.example.demospringcloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DemoSpringcloudZuulApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringcloudZuulApplication.class, args);
+	}
+
+}
